f0d5b269752d1c3ad1ae4dfe832c287755fa043e
Daniel Budris <d.c.budris@gmail.com>
2019-06-19 19:25:43 -0400

    Update trafficSniffer.py

--- trafficSniffer/trafficSniffer.py
+++ trafficSniffer/trafficSniffer.py
@@ -93,18 +93,15 @@ Top Path by Hits: {topPath}
         curses.cbreak()
         
         while True:
-            try:
-                # run the anomaly check
-                logging.debug("executing anaomaly check")
-                self.anomalyCheck()
-                stdscr.clear()
-                stdscr.addstr(self.generateStatusReport())
-                stdscr.refresh()
-                if not asDaemon:
-                    return()
-                sleep(frequency)
-            except KeyboardInterrupt:
-                pass    
+            # run the anomaly check
+            logging.debug("executing anaomaly check")
+            self.anomalyCheck()
+            stdscr.clear()
+            stdscr.addstr(self.generateStatusReport())
+            stdscr.refresh()
+            if not asDaemon:
+                return()
+            sleep(frequency)  
 
     def anomalyCheck(self, threshold=10, timeRange=2):
         # Obtain the number of hits in the specified timerange
