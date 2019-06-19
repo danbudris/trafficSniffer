4aa610ae09d96389457cde214c0af030505cabf5
Daniel Budris <d.c.budris@gmail.com>
2019-06-19 19:00:18 -0400

    Update anomalyDetectionTests.py

--- tests/anomalyDetectionTests.py
+++ tests/anomalyDetectionTests.py
@@ -26,7 +26,7 @@ class anomalyDetectionTest(unittest.TestCase):
         # Clear the data from the test sniffer dataframe 
         self.testSniffer.trafficData.iloc[0:0]
         self.testSniffer.trafficData.drop(self.testSniffer.trafficData.index[0:])
-        print(self.testSniffer.trafficData.iloc[0])
+        print(self.testSniffer.trafficData.iloc[1])
         print(self.testSniffer.trafficData)
         
     def testAnomalyAlertRecovery(self):
